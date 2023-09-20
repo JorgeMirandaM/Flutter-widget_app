3728cce90f4b55d1e20d8b3e47dcdc3989ab5627
Jorge Federico Miranda Morones <jorge100099@hotmail.com>
2023-09-20 11:23:09 -0600

    screens created and barrel updated

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+
+class InfiniteScrollScreen extends StatelessWidget {
+
+  static const name='Infinite_screen';
+
+  const InfiniteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
