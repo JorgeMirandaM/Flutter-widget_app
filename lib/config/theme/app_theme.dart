da3c7d078744875c7392f9d5f1cba6e8706d7846
JorgeMirandaM <jorge100099@hotmail.com>
2023-09-24 22:52:48 -0600

    before statenotifier

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -13,14 +13,16 @@ const colorList = <Color>[
 
 class AppTheme {
   final int selectColor;
+  final bool isDarkmode;
 
-  AppTheme({this.selectColor = 0})
+  AppTheme({this.selectColor = 0, this.isDarkmode=false})
       : assert(selectColor >= 0, 'Selected color must be greater than 0'),
         assert(selectColor < colorList.length,
             'Selected color must be less or equal than ${colorList.length - 1}');
 
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
+        brightness: isDarkmode ? Brightness.dark : Brightness.light,
         colorSchemeSeed: colorList[selectColor],
         appBarTheme: AppBarTheme(centerTitle: false)
       );
--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widget_app/config/router/app_router.dart';
 import 'package:widget_app/config/theme/app_theme.dart';
+import 'package:widget_app/presentation/providers/theme_provider.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+
+    final int selectColor =ref.watch(selectedColorProvider);
+
+    final bool isDarkmode = ref. watch(isDarkmodeProvider);
     return  MaterialApp.router(
       title: 'Flutter Widgets',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectColor: 2).getTheme(),
+      theme: AppTheme(selectColor: selectColor,isDarkmode:isDarkmode ).getTheme(),
       
     );
   }
